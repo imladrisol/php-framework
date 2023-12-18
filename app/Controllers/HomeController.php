4556cf14fe306fe83fe25fd775fe3efe9b85fefe
Olha Nevinchana <o.nevinchana@expoplatform.com>
2023-12-18 14:54:10 +0200

    add service to controller

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -4,13 +4,23 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
+use App\Services\YoutubeService;
 use Framework\Http\Response;
 
 final class HomeController
 {
+    private YoutubeService $youtubeService;
+
+    public function __construct(
+        YoutubeService $youtubeService
+    ) {
+        $this->youtubeService = $youtubeService;
+    }
+
     public function index(): Response
     {
         $content = '<h1>Hello, World</h1>';
+        $content .= '<a href="' . $this->youtubeService->getChannelUrl() . '">channel</a>';
 
         return new Response($content);
     }
