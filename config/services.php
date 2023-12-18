da52f7775a1322f83c92bdd667a6dc26c0411600
Olha Nevinchana <o.nevinchana@expoplatform.com>
2023-12-18 15:26:44 +0200

    add to service env value

--- config/services.php
+++ config/services.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use Framework\Http\Kernel;
 use League\Container\Argument\Literal\ArrayArgument;
+use League\Container\Argument\Literal\StringArgument;
 use League\Container\Container;
 use Framework\Routing\RouterInterface;
 use Framework\Routing\Router;
@@ -13,6 +14,7 @@ $routes = include BASE_PATH . '/routes/web.php';
 
 $container = new Container();
 $container->delegate(new ReflectionContainer(true));
+$container->add('APP_ENV', new StringArgument('local'));
 $container->add(RouterInterface::class, Router::class);
 $container->extend(RouterInterface::class)
     ->addMethodCall('registerRoutes', [new ArrayArgument($routes)]);
--- framework/src/Http/Kernel.php
+++ framework/src/Http/Kernel.php
@@ -10,11 +10,14 @@ use League\Container\Container;
 
 final class Kernel
 {
+    private string $appEnv = 'local';
+
     public function __construct(
         private RouterInterface $router,
         private Container $container
     )
     {
+        $this->appEnv = $this->container->get('APP_ENV');
     }
 
     public function handle(Request $request): Response
@@ -23,12 +26,23 @@ final class Kernel
             [$routerHandler, $vars] = $this->router->dispatch($request, $this->container);
 
             $response = call_user_func_array($routerHandler, $vars);
-        } catch (HttpException $e) {
-            $response = new Response($e->getMessage(), $e->getStatusCode());
-        } catch (\Throwable $e) {
-            $response = new Response($e->getMessage(), 500);
+        } catch (\Exception $e) {
+            $response = $this->createExceptionResponse($e);
         }
 
         return $response;
+    }
+
+    private function createExceptionResponse(\Throwable $e)
+    {
+        if (in_array($this->appEnv, ['local', 'testing'])) {
+            throw $e;
+        }
+
+        if ($e instanceof HttpException) {
+            return new Response($e->getMessage(), $e->getStatusCode());
+        }
+
+        return new Response($e->getMessage(), 500);
     }
 }
