2ac1adbb04fe28647a74a3d408c5bd1c5f531b52
Olha Nevinchana <o.nevinchana@expoplatform.com>
2023-12-19 08:32:27 +0200

    add console, migration

---
+++ database/migrations/20231219082248.php
@@ -0,0 +1,14 @@
+<?php
+
+return new class
+{
+    public function up(): void
+    {
+        echo __CLASS__ . ' method ' . __METHOD__ . PHP_EOL;
+    }
+
+    public function down(): void
+    {
+        echo __CLASS__ . ' method ' . __METHOD__ . PHP_EOL;
+    }
+};
